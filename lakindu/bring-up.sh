cf13723f32c2172603e17a34d11cfe8f597bec83
Lakindu Boteju <lakinduboteju@gmail.com>
2025-08-31 23:59:14 +0700

    fix: Issue in bring-up script

--- lakindu/bring-up.sh
+++ lakindu/bring-up.sh
@@ -3,4 +3,4 @@ cd ./docker
 git checkout -f v0.20.4
 
 # Use CPU for embedding and DeepDoc tasks:
-$ docker compose -f docker-compose-https.yml up -d
+docker compose -f docker-compose-https.yml up -d
