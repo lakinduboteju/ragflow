f4e5d8e0c3d7921a37d8edd66a141ea1e53e9aaf
Lakindu Boteju <lakinduboteju@gmail.com>
2025-09-17 20:31:30 +0700

    Minor adjustment to bring-up script

--- lakindu/bring-up.sh
+++ lakindu/bring-up.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd ./docker
-git checkout -f v0.20.4
+
+# No need to checkout
+# git checkout -f v0.20.4
 
 # Use CPU for embedding and DeepDoc tasks:
 docker compose -f docker-compose-https.yml up -d
